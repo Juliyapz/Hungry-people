c76c840161a888f99d74e421f0c4ae34fd0d9a16
Juliyapz <76488867+Juliyapz@users.noreply.github.com>
2022-02-22 19:54:34 +0900

    created specialties section

---
+++ scripts/slider.js
@@ -0,0 +1,14 @@
+const specSlider = new Swiper('.spec__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.spec__pagination',
+    bulletClass: ('spec__bullet'),
+    bulletActiveClass: ('spec__bullet--active'),
+  },
+
+  // autoplay: {
+  //   delay: 4000
+  // },
+
+});
